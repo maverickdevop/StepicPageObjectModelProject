895b7401107b044f59522b520162b34516e95a75
maverickdevop <57834199+maverickdevop@users.noreply.github.com>
2023-08-26 23:43:12 +0300

    Update test_product_page.py

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -52,7 +52,7 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
-@pytest.mark.xfail(reason='Negative check 3')
+@pytest.mark.xfail(reason='Negative check #3')
 @pytest.mark.negative
 def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, LINK, timeout=0)
